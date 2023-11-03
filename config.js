adb2741ca3c6a39292a6e9376dce8943c67a74b0
Birru Henok <henok.birru@nokia.com>
2023-11-03 18:09:05 +0100

    feat: #4 Setup and Initialize Firebase
    Firebase configuration with firebase-admin package added
    
    ref: https://github.com/APIExplore/api-explore-backend/issues/4

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
-require("dotenv").config();
-const firebase = require("firebase");
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-	measurementId: process.env.MEASUREMENT_ID,
-};
+require('dotenv').config();
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const serviceAccount = require('./creds.json');
+
+initializeApp({
+	credential: cert(serviceAccount),
+});
+const db = getFirestore();
+
+module.exports = { db };
