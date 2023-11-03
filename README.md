3aa05a9c337e5aa54d0631994be064e3a84bc919
Jofn9718004 <50374201+Jofn9718004@users.noreply.github.com>
2023-11-03 14:26:27 +0100

    Added fetch endpoint for the API schema
    Fetches the definition of API operations from the address supplied in the request body.
    
    Co-Authored-By: Gabriel Kazai <144999882+GabrielKazai@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # api-explore-backend
 An interactive demonstrator application which serves as a visualization tool for HTTP requests sent to an API service.
+# Endpoints
+ - POST /apischema/fetch
+    Fetches the API definition from supplied address
+    
+    Expected data in request body (JSON):
+    {
+        "address": "http://localhost:8080/swagger.json"
+    }
+
+    Responds with the API definition located at the URL in JSON format.
--- routes/apiSchema.js
+++ routes/apiSchema.js
@@ -4,16 +4,47 @@
 
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
+const fs = require('fs')
 
-router.post('/get', function (req, res, next) {
+router.post('/fetch', async function (req, res, next) {
   console.log('Getting API schema...')
 
-  // TODO:
-  //  - Extract URL of SUT from request (JSON)
-  //  - Call control function for getting API schema (/swagger.json)
-  //  - Send API schema in response if successful
+  // Check that valid request data is included
+  const reqData = req.body
+  if (!reqData || !reqData.address) {
+    return res.status(400).json({ error: 'Invalid request data', data: req.body })
+  }
 
-  res.send('API schema sent') // Placeholder
+  const url = reqData.address
+  try {
+    const response = await axios.get(url)
+
+    if (response.status === 200) {
+      const data = response.data
+
+      // Write API schema to a file (currently root directory)
+      const filePath = 'apiSchema.json'
+      fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+          console.error(' - Error writing to file:', err)
+          return res.status(500).json({ error: 'Failed to save API schema' })
+        }
+        console.log(' - API schema saved')
+        res.json(data) // Send API schema in response
+        console.log(' - API schema sent in response')
+      })
+    } else {
+      return res.status(response.status).json({ error: `Failed to fetch API schema from ${url}` })
+    }
+  } catch (error) {
+    console.error(` - Error fetching API schema from ${url}:`, error.message)
+    return res.status(500).json({ error: `Failed to fetch API schema from ${url}` })
+  }
+})
+
+router.post('/set', async function (req, res, next) {
+
 })
 
 module.exports = router
