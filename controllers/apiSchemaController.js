d20fce47b680cfa5a61af6caf07e18401496cd0c
Felix Bečeić <44879477+fbeceic@users.noreply.github.com>
2023-11-27 13:50:11 +0100

    feat: #18 Dependency Graph (#16)
    Added definitions to return value of schema json.
    
    ref: https://github.com/APIExplore/api-explore-frontend/issues/18

--- controllers/apiSchemaController.js
+++ controllers/apiSchemaController.js
@@ -1,36 +1,44 @@
 function getSchemaProperties (apiSchema) {
-  const result = {}
+  const result = {paths: {}, definitions: {}};
+
   try {
     if (apiSchema && apiSchema.paths) {
       // Extract all paths
       for (const path in apiSchema.paths) {
         const methods = apiSchema.paths[path]
-        result[path] = {}
+        result["paths"][path] = {}
 
         // Extract needed path properties
         for (const method in methods) {
           const operation = methods[method]
           if (operation) {
             if (operation.operationId) {
-              result[path][method] = { operationId: operation.operationId }
+              result["paths"][path][method] = { operationId: operation.operationId }
             } else {
-              result[path][method] = { operationId: path }
+              result["paths"][path][method] = { operationId: path }
             }
             if (operation.parameters) {
-              result[path][method].parameters = operation.parameters
+              result["paths"][path][method].parameters = operation.parameters
+            }
+            if (operation.responses && operation.responses["200"] && operation.responses["200"].schema && operation.responses["200"].schema.$ref) {
+              result["paths"][path][method].definitionRef = operation.responses["200"].schema.$ref
             }
           }
         }
       }
 
-      return result
+      if (apiSchema.definitions) {
+        result["definitions"] = apiSchema.definitions;
+      }
+
+      return result;
     }
   } catch (error) {
     console.error(
-      ' - Error extracting paths and methods from API schema: ',
-      error.message
-    )
-    return null
+        ' - Error extracting paths, methods and definitions from API schema: ',
+        error.message
+    );
+    return null;
   }
 }
 
